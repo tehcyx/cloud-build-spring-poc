7924ba7b15984915b1e8a6d6a158f3ba3fff9121
Daniel Roth <daniel.roth02@sap.com>
2018-12-10 13:51:41 -0800

    try caching gradle

--- cloud-build/integration-test/app/entrypoint.sh
+++ cloud-build/integration-test/app/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -e
 echo "## Starting tests"
-cd /workspace/
-gradle integrationTestJenkins
+./gradlew integrationTestJenkins
 
 echo "## Copying testresult"
 echo "here we can copy test results"
